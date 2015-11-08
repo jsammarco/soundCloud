5c83faef99b2b926c6ff5c1ba15f8c6e14015c78
Joseph Sammarco <joseph.sammarco@corecashless.com>
2015-11-07 20:12:40 -0600

    Serving public and started on UI

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-/*jslint node:true, browser:true, devel:true */
+/*jslint nomen:true, node:true, browser:true, devel:true */
 (function () {
     "use strict";
 
-    var express = require('express');
-    var app = express();
+    var express = require('express'),
+        app = express(),
+        host,
+        port,
+        server;
+
+    app.use('/', express.static(__dirname + '/public'));
 
     app.get('/', function (req, res) {
-      res.send('Hello World!');
+        res.send('Hello World!');
+        return req;
     });
 
-    var server = app.listen(3000, function () {
-      var host = server.address().address;
-      var port = server.address().port;
+    server = app.listen(3000, function () {
+        host = server.address().address;
+        port = server.address().port;
 
-      console.log('Example app listening at http://%s:%s', host, port);
+        console.log('Server at http://%s:%s', host, port);
     });
 
 }());
